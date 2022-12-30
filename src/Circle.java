2d20ed446bb72c70f6f09c7a494aa8ad142837e6
beka3301 <bekturhanazarov98@gmail.com>
2022-12-31 00:17:10 +0600

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+
+private static  final double radius=7.5;
+public static void  Area(){
+    double area =Math.PI*radius*radius;
+    System.out.println(area);
+}
+public static void Circumference(){
+double circumference=Math.PI*2*radius;
+    System.out.println(circumference);
+}
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.awt.geom.Area;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle=new Circle();
+        System.out.println("Area:");
+        Circle.Area();
+        System.out.println();
+        System.out.println("Circumference:");
+        Circle.Circumference();
+// я так сделал "-" 
+
+
+
+
+    }
+}
